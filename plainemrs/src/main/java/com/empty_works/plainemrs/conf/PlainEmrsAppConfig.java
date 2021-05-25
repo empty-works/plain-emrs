7167ef9b1fc9aae3431d07c12ffaed1fc96e7e4d
empty-works <pama.mark@gmail.com>
2021-05-25 21:12:00 +0200

    Set up view resolver to set the prefix and suffix for .jsp files. Also set up logger.

--- plainemrs/src/main/java/com/empty_works/plainemrs/conf/PlainEmrsAppConfig.java
+++ plainemrs/src/main/java/com/empty_works/plainemrs/conf/PlainEmrsAppConfig.java
@@ -1,8 +1,17 @@
 package com.empty_works.plainemrs.conf;
 
+import java.util.logging.Logger;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 @EnableWebMvc
@@ -10,4 +19,29 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 //@PropertySource("classpath:persistence-mysql.properties")
 public class PlainEmrsAppConfig {
 	
+	// Set up variable to hold the properties
+	@Autowired
+	private Environment env; // Will hold data read from properties files
+							 // Spring helper to hold the properties
+	
+	// Set up a logger for diagnostics
+	private Logger logger = Logger.getLogger(getClass().getName());
+	
+	// Define a bean for ViewResolver
+	@Bean
+	public ViewResolver resolveView() {
+		
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+		
+		viewResolver.setPrefix("/WEB-INF/view/");
+		viewResolver.setSuffix(".jsp");
+		
+		return viewResolver;
+	}
+	
+	// Define a bean for a security data source
+	//@Bean
+	//public DataSource getSecurityDataSource() {
+		
+	//}
 }
