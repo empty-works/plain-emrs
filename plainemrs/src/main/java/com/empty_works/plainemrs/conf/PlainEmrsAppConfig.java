8f71dff4c5f53cbb6df6c3bfafe7c5f92ea0d673
empty-works <pama.mark@gmail.com>
2021-05-25 20:33:10 +0200

    Packages for configuration annotations can now be properly imported.

--- plainemrs/src/main/java/com/empty_works/plainemrs/conf/PlainEmrsAppConfig.java
+++ plainemrs/src/main/java/com/empty_works/plainemrs/conf/PlainEmrsAppConfig.java
@@ -1,6 +1,8 @@
 package com.empty_works.plainemrs.conf;
 
-import java.beans.PropertyVetoException;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
 @EnableWebMvc
