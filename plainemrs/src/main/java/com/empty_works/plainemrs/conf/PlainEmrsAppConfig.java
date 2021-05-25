d8722cbdd1e1b20abda74894f9f7e3427c1cc7cd
empty-works <pama.mark@gmail.com>
2021-05-25 20:04:11 +0200

    Corrected issue with missing src/main/java resource folders.

---
+++ plainemrs/src/main/java/com/empty_works/plainemrs/conf/PlainEmrsAppConfig.java
@@ -0,0 +1,8 @@
+package com.empty_works.plainemrs.conf;
+
+import java.beans.PropertyVetoException;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages="com.empty_works.plainemrs")
+@PropertySource("classpath:persistence-mysql.properties")
