74f059dc5c877b1812d16a71c6677a079bc3bcb7
empty-works <pama.mark@gmail.com>
2021-06-14 04:04:25 +0200

    Created servlet and security web app initializers.

---
+++ plainemrs/src/main/java/com/empty_works/plainemrs/conf/PlainEmrsSecurityWebApplicationInitializer.java
@@ -0,0 +1,7 @@
+package com.empty_works.plainemrs.conf;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class PlainEmrsSecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+
+}
---
+++ plainemrs/src/main/java/com/empty_works/plainemrs/conf/PlainEmrsSpringMvcDispatcherServletInitializer.java
@@ -0,0 +1,24 @@
+package com.empty_works.plainemrs.conf;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class PlainEmrsSpringMvcDispatcherServletInitializer
+		extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return new Class[] {PlainEmrsAppConfig.class};
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		return new String[] { "/" };
+	}
+
+}
