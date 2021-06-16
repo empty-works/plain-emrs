8a146fe79923adbdf0833cd8f3240b140693b077
empty-works <pama.mark@gmail.com>
2021-06-16 19:51:57 +0200

    Created a main controller to display the main depending on the role of the user.

---
+++ plainemrs/src/main/java/com/empty_works/plainemrs/controller/MainController.java
@@ -0,0 +1,42 @@
+package com.empty_works.plainemrs.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+	@GetMapping("/")
+	public String showHome() {
+		
+		return "home";
+	}
+	
+	// Add request mapping for systems
+	@GetMapping("/systems")
+	public String showSystems() {
+		
+		return "systems";
+	}
+
+	// Add request mapping for medical staff
+	@GetMapping("/medical-staff")
+	public String showMedicalStaff() {
+		
+		return "medicalStaff";
+	}
+
+	// Add request mapping for allied staff
+	@GetMapping("/allied-staff")
+	public String showAlliedStaff() {
+		
+		return "alliedStaff";
+	}
+
+	// Add request mapping for non-employees
+	@GetMapping("/non-employees")
+	public String showNonEmployees() {
+		
+		return "nonEmployees";
+	}
+}
