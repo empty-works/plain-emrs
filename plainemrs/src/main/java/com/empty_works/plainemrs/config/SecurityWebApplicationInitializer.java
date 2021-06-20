627270de9449e1174a48a6c9f06ddcb600b27d25
empty-works <pama.mark@gmail.com>
2021-06-21 00:01:02 +0200

    Updated security web app initializer to try and fix spring security filter chain issue.

--- plainemrs/src/main/java/com/empty_works/plainemrs/config/SecurityWebApplicationInitializer.java
+++ plainemrs/src/main/java/com/empty_works/plainemrs/config/SecurityWebApplicationInitializer.java
@@ -2,6 +2,6 @@ package com.empty_works.plainemrs.config;
 
 import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
 
-public class PlainEmrsSecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
 
 }
