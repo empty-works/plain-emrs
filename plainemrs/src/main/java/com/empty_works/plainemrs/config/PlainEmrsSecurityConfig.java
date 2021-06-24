8fd0ae3d69a52ab131ee2d6e42c9ddc40abf744c
empty-works <pama.mark@gmail.com>
2021-06-24 04:40:25 +0200

    Updated pom file and changed login POST processing URL.

--- plainemrs/src/main/java/com/empty_works/plainemrs/config/PlainEmrsSecurityConfig.java
+++ plainemrs/src/main/java/com/empty_works/plainemrs/config/PlainEmrsSecurityConfig.java
@@ -29,15 +29,15 @@ public class PlainEmrsSecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		
 		http.authorizeRequests()
-			.antMatchers("/").hasRole("NON_MEDICAL_STAFF")
 			.antMatchers("/systems/**").hasRole("ADMIN")
 			.antMatchers("/medical-staff/**").hasAnyRole("DOCTOR", "NURSE")
 			.antMatchers("/allied-staff/**").hasRole("ALLIED_MEDICAL_STAFF")
 			.antMatchers("/non-employees/**").hasAnyRole("PROVIDER", "GOVERNMENT")
+			.antMatchers("/").hasRole("NON_MEDICAL_STAFF")
 			.and()
 			.formLogin()
-				.loginPage("/showMyLoginPage") // show custom form at the request mapping "/showMyLoginPage"
-				.loginProcessingUrl("/authenticateTheUser") // login form should POST data to this URL for processing 
+				.loginPage("/show-login-page") // show custom form at the request mapping "/showMyLoginPage"
+				.loginProcessingUrl("/authenticate-the-user") // login form should POST data to this URL for processing 
 															// (check user id and password)
 				.permitAll()// allow everyone to see login page. No need to be logged in.
 			.and()
