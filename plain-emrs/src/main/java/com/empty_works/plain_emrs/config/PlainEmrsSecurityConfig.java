45b0335a8380cec0e8d0cb7caf49fdab7fab63a5
empty-works <pama.mark@gmail.com>
2021-07-11 03:42:36 +0200

    Fixed missing datasource error.

--- plain-emrs/src/main/java/com/empty_works/plain_emrs/config/PlainEmrsSecurityConfig.java
+++ plain-emrs/src/main/java/com/empty_works/plain_emrs/config/PlainEmrsSecurityConfig.java
@@ -2,19 +2,19 @@ package com.empty_works.plain_emrs.config;
 
 import javax.sql.DataSource;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.User.UserBuilder;
 
 @Configuration
 @EnableWebSecurity
 public class PlainEmrsSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	// Add reference to security data source
+	@Autowired
 	private DataSource securityDataSource;
 
 	@Override
