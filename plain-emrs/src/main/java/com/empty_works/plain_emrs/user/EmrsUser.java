3eab666ab55d921eab54ae5444a2da31e261ffcc
empty-works <pama.mark@gmail.com>
2021-07-21 20:18:41 +0200

    Getters and setters for the User object.

--- plain-emrs/src/main/java/com/empty_works/plain_emrs/user/EmrsUser.java
+++ plain-emrs/src/main/java/com/empty_works/plain_emrs/user/EmrsUser.java
@@ -1,9 +1,15 @@
 package com.empty_works.plain_emrs.user;
 
+import java.util.Date;
+
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 public class EmrsUser {
+
+	@NotNull(message="is required")
+	@Size(min=1, message="is required")	
+	private int personId;
 
 	@NotNull(message="is required")
 	@Size(min=1, message="is required")	
@@ -12,9 +18,68 @@ public class EmrsUser {
 	@NotNull(message="is required")
 	@Size(min=1, message="is required")
 	private String password;
+	
+	@NotNull(message="is required")
+	@Size(min=1, message="is required")
+	private String emailAddress;
+	
+	@NotNull(message="is required")
+	@Size(min=1, message="is required")
+	private Date createdOn;
+
+	@NotNull(message="is required")
+	@Size(min=1, message="is required")
+	private boolean enabled;
+
 	public EmrsUser() {
 		
 	}
+
+	public int getPersonId() {
+		return personId;
+	}
+
+	public void setPersonId(int personId) {
+		this.personId = personId;
+	}
 	
+	public String getUserName() {
+		return userName;
+	}
 	
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public String getEmailAddress() {
+		return emailAddress;
+	}
+	
+	public void setEmailAddress(String emailAddress) {
+		this.emailAddress = emailAddress;
+	}
+
+	public Date getCreatedOn() {
+		return createdOn;
+	}
+	
+	public void setCreatedOn(Date createdOn) {
+		this.createdOn = createdOn;
+	}
+	
+	public boolean getEnabled() {
+		return enabled;
+	}
+
+	public void setEnabled(boolean enabled) {
+		this.enabled = enabled;
+	}
 }
