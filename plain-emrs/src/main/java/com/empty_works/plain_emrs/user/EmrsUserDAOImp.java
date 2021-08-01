3b64b7fbd904573339f34fa0c18e6386b0ac8aa7
empty-works <pama.mark@gmail.com>
2021-08-01 05:54:38 +0200

    Created implementation for the emrs user DAO.

---
+++ plain-emrs/src/main/java/com/empty_works/plain_emrs/user/EmrsUserDAOImp.java
@@ -0,0 +1,30 @@
+package com.empty_works.plain_emrs.user;
+
+import java.util.List;
+
+public class EmrsUserDAOImp implements EmrsUserDAO {
+
+	@Override
+	public List<EmrsUser> getEmrsUsers() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void saveEmrsUser(EmrsUser theUser) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public EmrsUser getEmrsUser(int theId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void deleteEmrsUser(int theId) {
+		// TODO Auto-generated method stub
+		
+	}
+}
