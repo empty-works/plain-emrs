771802c8cc684485b17f4419518334762da6ed96
empty-works <pama.mark@gmail.com>
2021-08-02 06:03:01 +0200

    Methods for emrs user implementation class.

--- plain-emrs/src/main/java/com/empty_works/plain_emrs/user/EmrsUserDAOImp.java
+++ plain-emrs/src/main/java/com/empty_works/plain_emrs/user/EmrsUserDAOImp.java
@@ -2,29 +2,49 @@ package com.empty_works.plain_emrs.user;
 
 import java.util.List;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class EmrsUserDAOImp implements EmrsUserDAO {
+	
+	@Autowired
+	private SessionFactory sessionFactory;
 
 	@Override
 	public List<EmrsUser> getEmrsUsers() {
-		// TODO Auto-generated method stub
-		return null;
+
+		Session currentSession = sessionFactory.getCurrentSession();
+		Query<EmrsUser> theQuery = currentSession.createQuery("from users order by username",
+				EmrsUser.class);
+		
+		List<EmrsUser> users = theQuery.getResultList();
+		
+		return users;
 	}
 
 	@Override
 	public void saveEmrsUser(EmrsUser theUser) {
-		// TODO Auto-generated method stub
 		
+		Session currentSession = sessionFactory.getCurrentSession();
+		currentSession.saveOrUpdate(theUser);
 	}
 
 	@Override
 	public EmrsUser getEmrsUser(int theId) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		Session currentSession = sessionFactory.getCurrentSession();
+		EmrsUser theUser = currentSession.get(EmrsUser.class, theId);
+		return theUser;
 	}
 
 	@Override
 	public void deleteEmrsUser(int theId) {
-		// TODO Auto-generated method stub
 		
+		Session currentSession = sessionFactory.getCurrentSession();
+
 	}
 }
