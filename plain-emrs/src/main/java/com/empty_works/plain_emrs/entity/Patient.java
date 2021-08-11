6f666dbc26117f64d60dbff3738c0e2878029b13
empty-works <pama.mark@gmail.com>
2021-08-11 20:39:04 +0200

    Updated data model patient ID primary to an integer instead of a string to allow for auto-incrementing.

--- plain-emrs/src/main/java/com/empty_works/plain_emrs/entity/Patient.java
+++ plain-emrs/src/main/java/com/empty_works/plain_emrs/entity/Patient.java
@@ -1,5 +1,13 @@
 package com.empty_works.plain_emrs.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="patients")
 public class Patient {
 
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="patient_id")
 }
