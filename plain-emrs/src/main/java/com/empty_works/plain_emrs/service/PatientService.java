d2ed7ef717cfe5d7f48ba99885596a765a714aa7
empty-works <pama.mark@gmail.com>
2021-08-12 20:44:56 +0200

    Implemented the patient service classes.

--- plain-emrs/src/main/java/com/empty_works/plain_emrs/service/PatientService.java
+++ plain-emrs/src/main/java/com/empty_works/plain_emrs/service/PatientService.java
@@ -1,5 +1,16 @@
 package com.empty_works.plain_emrs.service;
 
-public class PatientService {
+import java.util.List;
 
+import com.empty_works.plain_emrs.entity.Patient;
+
+public interface PatientService {
+
+	public List<Patient> getPatients();
+	
+	public void savePatient(Patient thePatient);
+	
+	public Patient getPatient(int theId);
+	
+	public void deletePatient(int theId);
 }
