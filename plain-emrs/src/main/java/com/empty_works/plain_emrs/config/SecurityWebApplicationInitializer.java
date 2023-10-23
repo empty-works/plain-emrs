e10c92f1126af643f2ce4ee8b74e1dbf706a8968
empty-works <58103998+empty-works@users.noreply.github.com>
2023-10-23 15:29:13 +0200

    Delete plain-emrs directory

--- plain-emrs/src/main/java/com/empty_works/plain_emrs/config/SecurityWebApplicationInitializer.java
+++
@@ -1,7 +0,0 @@
-package com.empty_works.plain_emrs.config;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
-
-}
